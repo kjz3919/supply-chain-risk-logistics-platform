b571cae8242464da02a276328f6aee5c079b2d00
kjz <jzych3919@gmail.com>
2026-07-19 14:36:43 +0200

    Added the feature of writing the raw data from bucket to data warehouse

--- ingestion/ingestion-script.py
+++ ingestion/ingestion-script.py
@@ -1,15 +1,39 @@
 import polars as pl
+from dotenv import load_dotenv
+import os
+from datetime import date
+todays_date = date.today()
 # Defining the bucket with raw data
 source = 's3://raw-scm-project/*.csv'
+
+# Loading in the environment file
+load_dotenv()
+# Defining credentials for connecting with S3 and Postgres
+
+# Credentials for S3 storage
+MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER")
+MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD")
+# Credentials for Postgres DWH
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+
 
 # Defining connection options for connection with minIO storage
 storage_options = {
     "aws_endpoint_url": "http://localhost:9000",
-    "aws_access_key_id": "kjz",
-    "aws_secret_access_key": "abcd12345",
+    "aws_access_key_id": MINIO_ROOT_USER,
+    "aws_secret_access_key": MINIO_ROOT_PASSWORD,
     "aws_region": "us-east-1"
 }
 
 # Downloading the data from CSV and storing it into a dataframe
 df = pl.scan_csv(source, storage_options=storage_options).collect()
 
+# Writing the raw data to Postgres
+df.write_database(
+    table_name="raw",
+    connection=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}",
+    if_table_exists="append"
+)
